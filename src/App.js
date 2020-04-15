6f9eb86af8c3d568fadf0ca6eeda0b0ec30859f7
douglas-he <doughrqe@gmail.com>
2020-04-15 16:17:32 -0300

    Pagina Inicial finalizada

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import Home from './Components/Home';
+import Receitas from './Components/Receitas';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route path="/receitas/:comidabebida/:id" component={Detalhes} />
         <Route path="/receitas" component={Receitas} />
+        {/* <Route path="/receitas/:comidabebida/:id" component={Detalhes} />
         <Route path="/receitas-feitas" component={ReceitasFeitas} />
         <Route path="/receitas-favoritas" component={ReceitasFavoritas} />
         <Route exact path="/explorar" component={Explorar} />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from "react-router-dom";
 
-function Home() {
+const Home = () => {
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [isRedirect, setIsRedirect] = useState(false);
+  const regexEmail = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  const isValidEmail = regexEmail.test(emailValue);
+  const isValidPassword = passwordValue.length > 5;
+
+  const onHandleSubmit = () => {
+    localStorage.setItem('meals-token', '1');
+    localStorage.setItem('cocktails-token', '1');
+    localStorage.setItem('user', JSON.stringify({ email: emailValue }));
+    setIsRedirect(true);
+  }
+
   return (
     <div>
-      <p>Home!</p>
+      <div>
+        <h1>Login</h1>
+      </div>
+      <div>
+        <input
+          type="email"
+          placeholder="Email"
+          data-testid="email-input"
+          value={emailValue}
+          onChange={({ target }) => setEmailValue(target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Senha"
+          data-testid="password-input"
+          value={passwordValue}
+          onChange={({ target }) => setPasswordValue(target.value)}
+        />
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={!(isValidPassword && isValidEmail)}
+          onClick={onHandleSubmit}
+        >
+          Entrar
+        </button>
+      </div>
+      {isRedirect &&<Redirect to='/receitas/comidas' />}
     </div>
   );
 }
