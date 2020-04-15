beb22e97ec9ada67f0e61065a66b70efacec21f6
richardwellerson <richardwellerson@gmail.com>
2020-04-15 17:03:02 -0300

    Code Climate

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,17 +9,17 @@ const Footer = () => (
   <div className="Footer_all">
     <Link to="/bebidas" >
       <div className="Footer_icon">
-        <img src={Drinks} alt='Drinks redirect' />
+        <img src={Drinks} alt="Drinks redirect" />
       </div>
     </Link>
     <Link to="/explorar">
       <div className="Footer_icon">
-        <img src={Explore} alt='Explore redirect' />
+        <img src={Explore} alt="Explore redirect" />
       </div>
     </Link>
     <Link to="/comidas">
       <div className="Footer_icon">
-        <img src={Meals} alt='Meals redirect' />
+        <img src={Meals} alt="Meals redirect" />
       </div>
     </Link>
   </div>
