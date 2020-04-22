b6f2c0e90553298a66602dffcb75479ec85c3341
douglas-he <doughrqe@gmail.com>
2020-04-22 19:32:13 -0300

    Diminuindo linhas de header

--- src/Components/Detalhes.js
+++ src/Components/Detalhes.js
@@ -3,6 +3,7 @@ import RecipesContext from '../Context';
 
 const Detalhes = () => {
   const { foodDetail } = useContext(RecipesContext);
+  console.log(foodDetail);
   return (<h1>Receitas</h1>)
 }
 export default Detalhes;
