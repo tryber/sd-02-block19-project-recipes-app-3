c94536b36feea61814eebbf595bc81fe296d6ac0
richardwellerson <richardwellerson@gmail.com>
2020-04-22 18:57:08 -0300

    Share criado copiando link da receita

--- src/Components/FavoriteAndShare.js
+++ src/Components/FavoriteAndShare.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Share from '../Images/Share.svg';
 import Favorite from '../Images/Favorite.svg';
 import '../Styles/FavoriteAndShare.css';
 
-const FavoriteAndShare = () => (
-  <div className="FavShare_father">
-    <div className="FavShare_content">
-      <img src={Share} alt="Share your recipe" />
+const favoriteRecipe = () => {
+  console.log('ia')
+}
+
+const FavoriteAndShare = () => {
+  const [copied, setCopied] = useState('');
+  const message = () => {
+    setCopied('Copied!');
+    setTimeout(() => {
+      setCopied('')
+    }, 2000);
+  }
+  return (
+    <div className="FavShare_father">
+      <CopyToClipboard
+        text={window.location.href}
+        onCopy={() => message()}
+      >
+        <div className="FavShare_content">
+          <img src={Share} alt="Share your recipe" />
+        </div>
+      </CopyToClipboard>
+      <div
+        className="FavShare_content"
+        onClick={() => favoriteRecipe()}
+      >
+        <img src={Favorite} alt="Favorite recipe" />
+      </div>
+      <p>{copied}</p>
     </div>
-    <div className="FavShare_content">
-      <img src={Favorite} alt="Favorite recipe" />
-    </div>
-  </div>
-);
+  );
+}
 
 export default FavoriteAndShare;
