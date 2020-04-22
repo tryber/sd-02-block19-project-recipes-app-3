fc8034a1f99058cf38d29312718dbc2041a38d81
richardwellerson <richardwellerson@gmail.com>
2020-04-22 19:37:39 -0300

    Criando logica para setar no LocalStorage

--- src/Components/FavoriteAndShare.js
+++ src/Components/FavoriteAndShare.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Share from '../Images/Share.svg';
 import Favorite from '../Images/Favorite.svg';
 import Favorited from '../Images/Favorited.svg';
+import RecipesContext from '../Context';
 import '../Styles/FavoriteAndShare.css';
+
+const heartClick = (isFavorite, setIsFavorite) => {
+  const { favoritedRecipes } = JSON.parse(localStorage.getItem('user'));
+  setIsFavorite(!isFavorite);
+  return isFavorite
+    ? favoritedRecipes = [...favoritedRecipes, foodDetails]
+    : favoritedRecipes.slice(0, favoritedRecipes.indexOf(foodDetails))
+      .concat(favoritedRecipes.slice(favoritedRecipes.indexOf(foodDetails) + 1)
+}
 
 const FavoriteAndShare = () => {
   const [copied, setCopied] = useState('');
-  const [favoritedRecipe, setFavoritedRecipe] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const { foodDetails } = useContext(RecipesContext);
   const message = () => {
     setCopied('Copied!');
     setTimeout(() => {
@@ -26,10 +37,10 @@ const FavoriteAndShare = () => {
       </CopyToClipboard>
       <div
         className="FavShare_content"
-        onClick={() => setFavoritedRecipe(!favoritedRecipe)}
+        onClick={() => heartClick(isFavorite, setIsFavorite, foodDetails)}
       >
         <img
-          src={favoritedRecipe ? Favorited : Favorite}
+          src={isFavorite ? Favorited : Favorite}
           alt="Favorite recipe" />
       </div>
       <p>{copied}</p>
