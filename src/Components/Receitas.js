c5ca9483be8327f932ef06cfeed72b006185eade
douglas-he <doughrqe@gmail.com>
2020-04-22 19:49:30 -0300

    CC

--- src/Components/Receitas.js
+++ src/Components/Receitas.js
@@ -17,35 +17,27 @@ const Receitas = () => {
   }, [window.location.href]);
   if (requestInitialPage === undefined) return (<h1>Nenhum Resultado</h1>);
   return (
-    !isFetching
-      ? fetchError ||
-      <div>
-        <Header />
-        {!noResults ? requestInitialPage.map((food, index) => {
-          const local = food.idDrink ? 'Drink' : 'Meal';
-          return (
-            index < 12
-              ? <Link
-                onClick={(() => setFoodDetail(food))}
-                to={(window.location.href.includes('comidas') ? `/receitas/comidas/${food.idMeal}` : `/bebidas/${food.idDrink}`)}
-                key={index}
-              >
-                <div>
-                  <img
-                    data-testid={`${index}-card-img`}
-                    alt={food[`str${local}`]}
-                    src={food[`str${local}Thumb`]}
-                  />
-                </div>
-                <p>{food.strCategory}</p>
-                <p data-testid={`${index}-card-name`} >{food[`str${local}`]}</p>
-              </Link>
-              : null
-          );
-        }) : <p>Sem Resultados</p>}
-        <Footer />
-      </div>
-      : <p>Loading</p>
+    !isFetching ? fetchError || <div><Header />
+      {!noResults ? requestInitialPage.map((food, index) => {
+        const local = food.idDrink ? 'Drink' : 'Meal';
+        return (
+          index < 12
+            ? <Link
+              onClick={(() => setFoodDetail(food))}
+              to={(window.location.href.includes('comidas') ? `/receitas/comidas/${food.idMeal}` : `/bebidas/${food.idDrink}`)}
+              key={index}
+            ><div>
+                <img
+                  data-testid={`${index}-card-img`}
+                  alt={food[`str${local}`]}
+                  src={food[`str${local}Thumb`]}
+                /></div>
+              <p>{food.strCategory}</p>
+              <p data-testid={`${index}-card-name`} >{food[`str${local}`]}</p>
+            </Link> : null
+        );
+      }) : <p>Sem Resultados</p>} <Footer />
+    </div> : <p>Loading</p>
   );
 };
 
