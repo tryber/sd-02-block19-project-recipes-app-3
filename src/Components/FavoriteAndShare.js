0dd4eb9ac7094a6fee15b54c00b823bdd9cb1a33
richardwellerson <richardwellerson@gmail.com>
2020-04-22 17:57:48 -0300

    Estrutura feita

--- src/Components/FavoriteAndShare.js
+++ src/Components/FavoriteAndShare.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Share from '../Images/Share.svg';
 import Favorite from '../Images/Favorite.svg';
+import '../Styles/FavoriteAndShare.css';
 
 const FavoriteAndShare = () => (
-  <div>
-    <img src={Favorite} alt="Favorite recipe" />
-    <img src={Share} alt="Share your recipe" />
+  <div className="FavShare_father">
+    <div className="FavShare_content">
+      <img src={Share} alt="Share your recipe" />
+    </div>
+    <div className="FavShare_content">
+      <img src={Favorite} alt="Favorite recipe" />
+    </div>
   </div>
 );
 
---
+++ src/Components/Receitas.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import FavoriteAndShare from './FavoriteAndShare';
+
+const Receitas = () => (
+  <FavoriteAndShare />
+);
+
+export default Receitas;
