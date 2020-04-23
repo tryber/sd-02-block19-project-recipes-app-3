7d4c16e2a2d95cf47260fa313e43d4e69de5922c
douglas-he <doughrqe@gmail.com>
2020-04-23 04:02:40 -0300

    Revert "Diminuindo linhas de header"
    This reverts commit b6f2c0e90553298a66602dffcb75479ec85c3341.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,36 +6,6 @@ import profilePicBtn from '../Images/profilePicBtn.png';
 import searchTopBtn from '../Images/searchTopBtn.png';
 import CategoryBar from './CategoryBar';
 
-const renderTitle = () => (
-  <div>
-    <Link to="/perfil">
-      <img data-testid="profile-top-btn" src={profilePicBtn} alt="profile button" />
-    </Link>
-    <h2 data-testid="page-title">{
-      window.location.href.includes('comidas')
-        ? 'Comidas'
-        : 'Bebidas'
-    }
-    </h2>
-  </div>
-);
-const renderInputImage = (setVisibleSearch, visibleSearch) => (
-  <input
-    type="image"
-    data-testid="search-top-btn"
-    src={searchTopBtn}
-    alt="search top button"
-    onClick={() => setVisibleSearch(!visibleSearch)}
-  />
-);
-const returnDebounce = (searchCriteria, inputChange) => (
-  <DebounceInput
-    disabled={!searchCriteria}
-    debounceTimeout={600}
-    onChange={(e) => inputChange(e.target.value)}
-    data-testid="search-input"
-  />
-);
 export default function Header() {
   const [searchCriteria, setSearchCriteria] = useState('');
   const [input, setInput] = useState('');
@@ -49,7 +19,7 @@ export default function Header() {
     setRequestInitialPage([]);
     defineSearch(iValue, searchCriteria);
   }
-
+  
   const radioChange = (rValue) => {
     setSearchCriteria(rValue);
     if (input !== '') defineSearch(input, rValue);
@@ -57,7 +27,15 @@ export default function Header() {
 
   return (
     <div className="header">
-      {renderTitle()}
+      <Link to="/perfil">
+        <img data-testid="profile-top-btn" src={profilePicBtn} alt="profile button" />
+      </Link>
+      <h2 data-testid="page-title">{
+        window.location.href.includes('comidas')
+          ? 'Comidas'
+          : 'Bebidas'
+      }
+      </h2>
       <input
         type="image"
         data-testid="search-top-btn"
@@ -74,18 +52,27 @@ export default function Header() {
           data-testid="search-input"
         />
         <div className="searchRecipes">
-          {arrayRadio.map((ele, index) => (
-            <div key={ele}>
-              <input
-                type="radio"
-                name="recipeSearch"
-                value={arrayValueRadio[index]}
-                onClick={(e) => radioChange(e.target.value)}
-                id={ele}
-              />
-              <label htmlFor={ele}>{ele}</label>
-            </div>
-          ))}
+          <input
+            type="radio"
+            name="recipeSearch"
+            value="/filter.php?i="
+            onClick={(e) => radioChange(e.target.value)}
+          />
+          <label htmlFor="ingredient">Ingrediente</label>
+          <input
+            type="radio"
+            name="recipeSearch"
+            value="/search.php?s="
+            onClick={(e) => radioChange(e.target.value)}
+          />
+          <label htmlFor="name">Nome</label>
+          <input
+            type="radio"
+            name="recipeSearch"
+            value="/search.php?f="
+            onClick={(e) => radioChange(e.target.value)}
+          />
+          <label htmlFor="firstLetter">Primeira letra</label>
         </div>
       </form>}
     </div>
