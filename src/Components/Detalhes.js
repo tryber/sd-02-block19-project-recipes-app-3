a9e26a66b6181053053c748b92f3732195cbe4ca
douglas-he <doughrqe@gmail.com>
2020-04-23 03:31:07 -0300

    test

--- src/Components/Detalhes.js
+++ src/Components/Detalhes.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import RecipesContext from '../Context';
 
+// teste
+
 const Detalhes = () => {
   const { foodDetail } = useContext(RecipesContext);
   console.log(foodDetail);
