2fcb34d1afd424bbdf5dbfc91050f148850af1e7
richardwellerson <richardwellerson@gmail.com>
2020-04-27 16:23:44 -0300

    ingredientes feitos

--- src/Components/DetailsIngredients.js
+++ src/Components/DetailsIngredients.js
@@ -4,20 +4,38 @@ import RecipesContext from '../Context/';
 const DetailsIngredients = () => {
   const { foodObject } = useContext(RecipesContext);
   const isFood = foodObject.meals[0] || foodObject.drinks[0]
-  console.log(isFood);
-  const allDataFood = Object.keys(isFood).filter((food) => (
+  const isIngredient = Object.keys(isFood).filter((food) => (
     food.includes('Ingredient')
-    && isFood[food].split('').length > 1
-    || food.includes('Measure')
-    && isFood[food].split('').length > 1
+    && isFood[food].split('').length >= 1
   ));
-  console.log(allDataFood);
+  const isMeasure = Object.keys(isFood).filter((food) => (
+    food.includes('Measure')
+    && isFood[food].split('').length >= 1
+  ));
   return (
     <div>
-      <p>Teste</p>
-      <p></p>
+      {isIngredient
+        &&
+        <div>
+          {isIngredient.map((ingredient, index) => {
+            const ingredientToShow = isFood[ingredient];
+            const measureToShow = isFood[isMeasure[index]]
+            return (
+              <div>
+                <span data-testid={`${index}-ingredient-name`}>
+                  {ingredientToShow}
+                </span>
+                -
+                <span data-testid={`${index}-ingredient-measure`}>
+                  {measureToShow}
+                </span>
+              </div>
+            )
+          })}
+        </div>
+      }
     </div>
-  )
+  );
 }
 
 export default DetailsIngredients;
