38d0fe44350914db22577fd2438650d807d3de9a
douglas-he <doughrqe@gmail.com>
2020-05-03 20:57:55 -0300

    Consertando Travis

--- src/tests/footer.test.js
+++ src/tests/footer.test.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import Footer from '../Components/Footer';
+import AppProvider from '../Context/AppProvider';
 
 afterEach(cleanup);
 
-describe('Test footer', ()=> {
+describe('Test footer', () => {
   it('render footer', () => {
     const { getByTestId } = render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>,
+      <AppProvider>
+        <MemoryRouter>
+          <Footer />
+        </MemoryRouter>
+      </AppProvider>,
     );
     expect(getByTestId('drinks-bottom-btn')).toBeInTheDocument();
     expect(getByTestId('explore-bottom-btn')).toBeInTheDocument();
