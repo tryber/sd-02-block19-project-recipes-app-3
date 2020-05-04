0d176c70c3cce614081c3e842e620b81d25e55a7
douglas-he <doughrqe@gmail.com>
2020-05-04 11:38:24 -0300

    CC

--- src/Pages/ReceitasFavoritas.js
+++ src/Pages/ReceitasFavoritas.js
@@ -1,37 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { renderButtons } from './ReceitasFeitas';
+import { renderButtons, renderCard } from './ReceitasFeitas';
 import Favorited from '../Images/Favorited.svg';
 import HeaderPerfil from '../Components/HeaderPerfil';
-import CopyButton from '../Components/CopyButton';
 
-
-const renderCard = (food, type, index, setCopyFav, favoriteRecipes, boolean) => (
-  <div>
-    <Link to={`/receitas/${food.idMeal ? 'comidas' : 'bebidas'}/${food[`id${type}`]}`}>
-      <img
-        data-testid={`${index}-horizontal-image`}
-        alt={food[`str${type}`]}
-        src={food[`str${type}Thumb`]}
-      />
-      <p data-testid={`${index}-horizontal-name`}>{food[`str${type}`]}</p>
-    </Link>
-    {boolean && <p>{food.strCategory}</p>}
-    {boolean && <p>{food.strArea}</p>}
-    {!boolean && <p>{food.strAlcoholic}</p>}
-    <CopyButton
-      url={`${window.location.origin}/receitas/${food.idMeal ? 'comidas' : 'bebidas'}/${food[`id${type}`]}`}
-    />
-    <input
-      data-testid={`${index}-horizontal-favorite-btn`}
-      type="image"
-      src={Favorited}
-      alt="heart"
-      onClick={() => setCopyFav(favoriteRecipes.filter(({ idMeal, idDrink }) => (
-        food.idDrink !== idDrink || food.idMeal !== idMeal)))}
-    />
-  </div>
-)
 const ReceitasFeitas = () => {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
   const [copyFav, setCopyFav] = useState([]);
@@ -43,7 +14,7 @@ const ReceitasFeitas = () => {
   useEffect(() => {
     setFavoriteRecipes([...copyFav]);
     localStorage.setItem('favorite-recipes', JSON.stringify(copyFav));
-  }, [copyFav])
+  }, [copyFav]);
   return (
     <div >
       <HeaderPerfil />
@@ -51,7 +22,19 @@ const ReceitasFeitas = () => {
       <div className="containCards">
         {favoriteRecipes.map((food, index) => {
           const type = food.idMeal ? 'Meal' : 'Drink';
-          return renderCard(food, type, index, setCopyFav, favoriteRecipes, food.idMeal ? true : false);
+          return (
+            <div>
+              {renderCard(index, food, type)}
+              <input
+                data-testid={`${index}-horizontal-favorite-btn`}
+                type="image"
+                src={Favorited}
+                alt="heart"
+                onClick={() => setCopyFav(favoriteRecipes.filter(({ idMeal, idDrink }) => (
+                  food.idDrink !== idDrink || food.idMeal !== idMeal)))}
+              />
+            </div>
+          )
         })}
       </div>
     </div>);
